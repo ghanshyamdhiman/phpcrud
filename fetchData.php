6ead08da91f79591692f55b81c7baea9fe97f7ea
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2023-12-17 10:09:32 +0530

    Update fetchData.php

--- fetchData.php
+++ fetchData.php
@@ -11,37 +11,35 @@ $dbDetails = array(
 ); 
  
 // DB table to use 
-$table = 'members'; 
+$table = 'tblfsr'; 
  
 // Table's primary key 
-$primaryKey = 'id'; 
+$primaryKey = 'fsr_id'; 
  
 // Array of database columns which should be read and sent back to DataTables. 
 // The `db` parameter represents the column name in the database.  
 // The `dt` parameter represents the DataTables column identifier. 
 $columns = array( 
-    array( 'db' => 'first_name', 'dt' => 0 ), 
-    array( 'db' => 'last_name',  'dt' => 1 ), 
-    array( 'db' => 'email',      'dt' => 2 ), 
-    array( 'db' => 'gender',     'dt' => 3 ), 
-    array( 'db' => 'country',    'dt' => 4 ), 
+    array( 'db' => 'fsr_id', 'dt' => 0),
+    array( 'db' => 'fsr_date', 'dt' => 1,'formatter' => function( $d, $row ) {return date( 'jS M Y', strtotime($d));}),
+    array( 'db' => 'fsr_slot', 'dt' => 2),
+    array( 'db' => 'fsr_sch_fm', 'dt' => 3,'formatter' => function( $d, $row ) {return date( 'h:i:s', strtotime($d));}),
+    array( 'db' => 'fsr_sch_till', 'dt' => 4,'formatter' => function( $d, $row ) {return date( 'h:i:s', strtotime($d));}),
+    array( 'db' => 'faculty', 'dt' => 5),
+    array( 'db' => 'lec_type', 'dt' => 6),
+    array( 'db' => 'act_code', 'dt' => 7),
+    array( 'db' => 'batch', 'dt' => 8),
+    array( 'db' => 'room', 'dt' => 9),
+    array( 'db' => 'chap_code', 'dt' => 10),
+    array( 'db' => 'fac_time_in', 'dt' => 11,'formatter' => function( $d, $row ) {return date( 'h:i:s', strtotime($d));}),
+    array( 'db' => 'fac_time_out', 'dt' => 12,'formatter' => function( $d, $row ) {return date( 'h:i:s', strtotime($d));}),
+    array( 'db' => 'attn_by', 'dt' => 13),
+    array( 'db' => 'attn_by_in', 'dt' => 14,'formatter' => function( $d, $row ) {return date( 'h:i:s', strtotime($d));}),
+    array( 'db' => 'attn_by_out', 'dt' => 15,'formatter' => function( $d, $row ) {return date( 'h:i:s', strtotime($d));}),
+    array( 'db' => 'remarks', 'dt' => 16),
     array( 
-        'db'        => 'created', 
-        'dt'        => 5, 
-        'formatter' => function( $d, $row ) { 
-            return date( 'jS M Y', strtotime($d)); 
-        } 
-    ), 
-    array( 
-        'db'        => 'status', 
-        'dt'        => 6, 
-        'formatter' => function( $d, $row ) { 
-            return ($d == 1)?'Active':'Inactive'; 
-        } 
-    ), 
-    array( 
-        'db'        => 'id', 
-        'dt'        => 7, 
+        'db'        => 'fsr_id', 
+        'dt'        => 17, 
         'formatter' => function( $d, $row ) { 
             return ' 
                 <a href="javascript:void(0);" class="btn btn-warning" onclick="editData('.htmlspecialchars(json_encode($row), ENT_QUOTES, 'UTF-8').')">Edit</a>&nbsp; 
